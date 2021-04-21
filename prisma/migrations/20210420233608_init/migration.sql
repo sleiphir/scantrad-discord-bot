f2595d4aae308bb3c726843621ed20a43111020f
Simon Lasbrugnas <simon.lasbrugnas@gmail.com>
2021-04-21 18:51:18 +0200

    remove migrations from the repo

--- prisma/migrations/20210420233608_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[title]` on the table `Manga` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Manga.title_unique" ON "Manga"("title");
--- prisma/migrations/20210421160602_1_0/migration.sql
+++
@@ -1,2 +0,0 @@
--- DropIndex
-DROP INDEX "Guild.channel_id_unique";
