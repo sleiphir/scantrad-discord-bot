9cf3204eb77a95eb9a008af3a7f63f93088ffa43
Simon Lasbrugnas <simon.lasbrugnas@gmail.com>
2021-04-21 19:04:57 +0200

    Fix ShowUseFollowListCommand

--- src/command/ShowUserFollowListCommand.ts
+++ src/command/ShowUserFollowListCommand.ts
@@ -7,7 +7,7 @@ export class ShowUserFollowListCommand extends Command {
 
         const db = new DB();
         const list = await db.getUserFollowList(this.context.guild.id, this.context.author.id)
-        if (list.length > 0) {
+        if (list?.length > 0) {
             this.context.reply(`${list.map(elem => elem.mangaTitle).join(', ')}`);
         } else {
             this.context.reply(`you don't follow anyting yet.`)
