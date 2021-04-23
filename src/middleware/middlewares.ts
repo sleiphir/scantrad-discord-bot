760a44b7629e378137df2f114a6dd9621db001f1
Simon Lasbrugnas <simon.lasbrugnas@gmail.com>
2021-04-23 20:57:25 +0200

    tabulation

--- src/middleware/middlewares.ts
+++ src/middleware/middlewares.ts
@@ -6,13 +6,13 @@ import { AtLeast3CharactersMiddleware } from "./AtLeast3CharactersMiddleware";
 
 // Bind middlewares to the corresponding commands
 const middlewares = {
-    SetFeedChannelCommand: [AdminMiddleware],
-    NewMangaNotificationCommand: [AdminMiddleware, ContentIsBooleanMiddleware],
-    ShowHelperCommand: [],
-    MangaListCommand: [],
-    ShowUserFollowListCommand: [NotificationChannelMiddleware],
-    SubscriptionCommand: [NotificationChannelMiddleware, AtLeast3CharactersMiddleware],
-    UnsubscriptionCommand: [NotificationChannelMiddleware, FollowListNotEmptyMiddleware, AtLeast3CharactersMiddleware],
+    SetFeedChannelCommand:          [AdminMiddleware],
+    NewMangaNotificationCommand:    [AdminMiddleware, ContentIsBooleanMiddleware],
+    ShowHelperCommand:              [],
+    MangaListCommand:               [],
+    ShowUserFollowListCommand:      [NotificationChannelMiddleware],
+    SubscriptionCommand:            [NotificationChannelMiddleware, AtLeast3CharactersMiddleware],
+    UnsubscriptionCommand:          [NotificationChannelMiddleware, FollowListNotEmptyMiddleware, AtLeast3CharactersMiddleware],
 };
 
 export default middlewares;
